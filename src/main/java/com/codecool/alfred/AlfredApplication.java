268a3af2675344eb4aa9ed0cc8a2e7310acca49a
laszloOrha <orha.laszlo.93@gmail.com>
2019-02-06 12:53:16 +0100

    initial commit

---
+++ src/main/java/com/codecool/alfred/AlfredApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.alfred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfredApplication.class, args);
+    }
+
+}
+
